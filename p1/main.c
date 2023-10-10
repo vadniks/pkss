996e71bae96c0c0858e199dc4f5fcccced7e9283
vadniks <whatever@fake.some>
2023-10-10 15:06:56 +0300

    pr1

--- p1/main.c
+++ p1/main.c
@@ -2,149 +2,140 @@
 #pragma clang diagnostic ignored "-Wgnu-folding-constant"
 
 #include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ncurses.h>
-//#include <form.h>
-
-typedef enum : unsigned {
-    SCREEN_HOME = 255,
-    SCREEN_T19 = 19,
-    SCREEN_T22 = 22,
-    SCREEN_T25 = 25,
-    SCREEN_T28 = 28,
-    SCREEN_T3 = 3
-} Screens;
-
-static Screens gScreen = SCREEN_HOME;
-static const unsigned gBufferSize = 5;
-static char gBuffer[gBufferSize] = { 0 };
-static unsigned gBufferPosition = 0;
-static unsigned gCursorX = 0;
-static unsigned gCursorY = 0;
-static unsigned gFieldsState = 0;
-static const unsigned gLargeBufferSize = 0xff;
-
-//static float maxf(float a, float b)
-//{ return a > b ? a : b; }
+#include <time.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 static bool t19(float a, float b, float c, float d)
 { return a * b <= c * d && a <= c && b <= d; }
 
-#define width (stdscr->_maxx)
-#define height (stdscr->_maxy)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static void drawScreen(void (*screen)(void)) {
-    clear();
-    box(stdscr, '|', '-');
-    refresh();
+typedef enum : int {
+    PAGE_ERROR = 0,
+    PAGE_MAIN = 1,
+    PAGE_T19 = 2
+} Pages;
 
-    (*screen)();
+static Pages gPage = PAGE_MAIN;
+
+static SDL_Window* gWindow = NULL;
+static SDL_Renderer* gRenderer = NULL;
+static TTF_Font* gFont = NULL;
+
+static int gWidth = 0, gHeight = 0;
+
+static const int gBufferSize = 255;
+static char gBuffer[gBufferSize];
+static int gBufferPosition = 0;
+
+static SDL_Texture* makeTextTexture(const char* text, SDL_Color color) {
+    SDL_Surface* surface = TTF_RenderText_Solid(gFont, text, color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(gRenderer, surface);
+    SDL_FreeSurface(surface);
+    return texture;
 }
 
-static void homeScreen(void) {
-    const char* title = "Enter task number or q to exit";
-
-    gCursorX = width / 2 - strlen(title) / 2;
-    gCursorY = height / 2 - 1;
-
-    move(gCursorY, gCursorX);
-    printw("%s: %.*s", title, gBufferPosition, gBuffer);
-
-    refresh();
+static void drawText(SDL_Rect rect, const char* text, SDL_Color color) {
+    SDL_Texture* texture = makeTextTexture(text, color);
+    SDL_RenderCopy(gRenderer, texture, NULL, &rect);
+    SDL_DestroyTexture(texture);
 }
 
-static void t19Screen(void) {
-    static char edit1[gBufferSize], edit2[gBufferSize];
-    static unsigned edit1Position, edit2Position;
+static inline SDL_Color colorWhite(void) { return (SDL_Color) {255, 255, 255, 255}; }
+static inline SDL_Color colorBlack(void) { return (SDL_Color) {0, 0, 0, 0}; }
+static inline SDL_Color colorDefault(void) { return (SDL_Color) {30, 34, 41, 0}; }
 
-    if (edit1Position > gBufferSize || edit2Position > gBufferSize) {
-        memset(edit1, 0, gBufferSize);
-        memset(edit2, 0, gBufferSize);
-        edit1Position = 0;
-        edit2Position = 0;
+static void drawMainPage(void) {
+    drawText((SDL_Rect) {10, 10, 500, 100}, "Hello World!", colorWhite());
+}
+
+static void drawPage(void) {
+    SDL_Color color = colorDefault();
+    SDL_SetRenderDrawColor(gRenderer, color.r, color.g, color.b, color.a);
+
+    SDL_RenderClear(gRenderer);
+
+    switch (gPage) {
+        case PAGE_MAIN:
+            drawMainPage();
+            break;
     }
 
-    char field1[gLargeBufferSize] = { 0 };
-    const int field1Size = snprintf(field1, gLargeBufferSize, "Enter A and B (in form of 1,2): "
-                                                              "%.*s", edit1Position, edit1);
-    const int field1Y = height / 3 - 1, field1X = width / 3;
-    move(field1Y, field1X);
-    mvprintw(field1Y, field1X, "%s", field1);
-    refresh();
-
-    char field2[gLargeBufferSize] = { 0 };
-    const int field2Size = snprintf(field2, gLargeBufferSize, "Enter C and D (in form of 1,2): "
-                                                              "%.*s", edit2Position, edit2);
-    const int field2Y = height - height / 3, field2X = width / 3;
-    mvprintw(field2Y, field2X, "%s", field2);
-    refresh();
-
-    if (gFieldsState == 0) {
-        memcpy(edit1, gBuffer, gBufferSize);
-        edit1Position = gBufferPosition;
-        move(field1Y, field1X + field1Size);
-    } else if (gFieldsState == 1) {
-        memcpy(edit2, gBuffer, gBufferSize);
-        edit2Position = gBufferPosition;
-        move(field2Y, field2X + field2Size);
-    } else if (gFieldsState == 2) {
-        mvprintw(height / 2 - 1, width / 3 - 3, "Result: %f", t19(1, 2, 3, 4));
-    } else
-        gFieldsState = 0;
+    SDL_RenderPresent(gRenderer);
 }
 
-static void errorScreen(void) {
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    attron(COLOR_PAIR(1));
-
-    const char* title = "An error has occurred, press q to exit";
-    mvprintw(height / 2 - 1, width / 2 - strlen(title) / 2, "%s", title);
-
-    attroff(COLOR_PAIR(1));
-    refresh();
-}
-
-static void draw(void) { switch (gScreen) {
-    case SCREEN_HOME: drawScreen(&homeScreen); break;
-    case SCREEN_T19: drawScreen(&t19Screen); break;
-    case SCREEN_T22: break;
-    case SCREEN_T25: break;
-    case SCREEN_T28: break;
-    case SCREEN_T3: break;
-    default: drawScreen(&errorScreen); break;
-} }
-
-static void enterPressed(void) {
-    if (gScreen == SCREEN_HOME)
-        gScreen = (Screens) atoi(gBuffer);
-    else
-        gFieldsState++;
-
-    memset(gBuffer, 0, gBufferSize);
-    gBufferPosition = 0;
+static void keyPressed(const SDL_Event* event) {
+    const SDL_KeyCode keyCode = event->key.keysym.sym;
+    switch (keyCode) {
+        case SDLK_BACKSPACE:
+            if (gBufferPosition > 0)
+                gBuffer[gBufferPosition--] = 0;
+            break;
+        case SDLK_RETURN:
+            // TODO
+            break;
+        default:
+            if (
+                (keyCode >= SDLK_0 && keyCode <= SDLK_9)
+                || (keyCode >= SDLK_a && keyCode <= SDLK_z)
+                && gBufferPosition < gBufferSize
+            )
+                gBuffer[gBufferPosition++] = (char) keyCode;
+            break;
+    }
 }
 
 int main(void) {
-    initscr();
-    noecho();
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) abort();
+    TTF_Init();
 
-    int input;
-    do {
-        draw();
+    gWindow = SDL_CreateWindow(
+        "pkss 1",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        16 * 50,
+        9 * 50,
+        SDL_WINDOW_SHOWN
+    );
+    if (!gWindow) abort();
 
-        if ((input = getch()) == 127)
-            gBuffer[gBufferPosition ? gBufferPosition-- : 0] = 0;
-        else if (input == 10)
-            enterPressed();
-        else if (input >= '0' && input <= '9' || input >= 'a' && input <= 'z')
-            gBuffer[gBufferPosition < gBufferSize ? gBufferPosition++ : gBufferPosition] = (char) input;
-        else
-            exit(input);
+    gRenderer = SDL_CreateRenderer(
+        gWindow,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if (!gRenderer) abort();
 
-    } while (input != 'q');
+    SDL_GetRendererOutputSize(gRenderer, &gWidth, &gHeight);
 
-    endwin();
-    return EXIT_SUCCESS;
+    gFont = TTF_OpenFont("RobotoMono-Regular.ttf", 500);
+
+    while (true) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: goto end;
+                case SDL_KEYDOWN: keyPressed(&event); break;
+                default: break;
+            }
+        }
+
+        drawPage();
+        nanosleep(&((struct timespec) {
+            0,
+            (int) 1e3f / 60 * (int) 1e6f
+        }), NULL);
+    }
+
+    end:
+    TTF_CloseFont(gFont);
+
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+
+    TTF_Quit();
+    SDL_Quit();
+    return 0;
 }
