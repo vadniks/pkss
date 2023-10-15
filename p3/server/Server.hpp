1e4034d006d8421e51491444b9ab9a88b8910f37
vadniks <whatever@fake.some>
2023-10-15 23:33:51 +0300

    pr3

--- p3/server/Server.hpp
+++ p3/server/Server.hpp
@@ -8,9 +8,13 @@
 #include <QtCore/QDebug>
 #include <QtCore/QJsonObject>
 #include <QtCore/QJsonDocument>
+#include <QtCore/QCoreApplication>
 #include "Tasks.hpp"
 
-#undef assert
+#ifdef assert
+#   undef assert
+#endif
+
 static void assert(bool a) { if (!a) throw QException(); }
 
 class Server final : public QObject {
@@ -24,6 +28,17 @@ public:
         connect(&mServer, &QTcpServer::newConnection, this, &Server::newConnection);
         assert(mServer.listen(QHostAddress::LocalHost, 8080));
         qInfo() << "Server started on " << mServer.serverAddress() << ':' << mServer.serverPort();
+    }
+
+    ~Server() override {
+        for (auto i : mSockets) {
+            i->close();
+            delete i;
+        }
+
+        mServer.close();
+        qInfo() << "Stopping server...";
+        QCoreApplication::quit();
     }
 
 private:
@@ -41,6 +56,10 @@ private:
         return list;
     }
 
+    static inline QString peerAddressAndPort(const QTcpSocket* socket) {
+        return QString().append(socket->peerAddress().toString()).append(':').append(socket->peerPort());
+    }
+
 private slots:
     void newConnection() {
         while (mServer.hasPendingConnections()) {
@@ -48,7 +67,7 @@ private slots:
             connect(socket, &QTcpSocket::readyRead, this, [this, socket](){ socketReadyToRead(socket); });
             connect(socket, &QTcpSocket::disconnected, this, [this, socket](){ socketDisconnected(socket); });
             mSockets.insert(socket);
-            qInfo() << "connected client " << socket->peerAddress() << ':' << socket->peerPort();
+            qInfo() << "connected client " << peerAddressAndPort(socket);
         }
     };
 
@@ -72,10 +91,17 @@ private slots:
             case 3:
                 outputJson["result"] = listToJsonArray(Tasks::t3(jsonArrayToList(inputJson["parameters"].toArray())));
                 break;
+            case 0:
+                qInfo() << "received stopping signal from client " << peerAddressAndPort(socket);
+                socket->disconnectFromHost();
+                mServer.close();
+                break;
+            default:
+                break;
         }
 
         if (socket->write(QJsonDocument(outputJson).toJson()) == 0) {
-            qInfo() << "disconnecting client " << socket->peerAddress() << ' ' << socket->peerPort() << " in cause of write error...";
+            qInfo() << "disconnecting client " << peerAddressAndPort(socket) << " in cause of write error...";
             mSockets.remove(socket);
             socket->close();
             delete socket;
@@ -83,7 +109,7 @@ private slots:
     }
 
     void socketDisconnected(QTcpSocket* socket) {
-        qInfo() << "disconnected client " << socket->peerAddress() << ' ' << socket->peerPort();
+        qInfo() << "disconnected client " << peerAddressAndPort(socket);
         socket->close();
         mSockets.remove(socket);
         delete socket;
--- p3/server/main.cpp
+++ p3/server/main.cpp
@@ -1,6 +1,5 @@
 
 #include "Server.hpp"
-#include <QtCore/QCoreApplication>
 
 int main(int argc, char** argv) {
     QCoreApplication app(argc, argv);
