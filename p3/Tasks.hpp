1f201047f04abbdaf9bb37d69d26e8bca160704d
vadniks <whatever@fake.some>
2023-10-15 22:30:08 +0300

    pr3

--- p3/Tasks.hpp
+++ p3/Tasks.hpp
@@ -1,21 +1,33 @@
 
 #pragma once
 
+#pragma ide diagnostic ignored "cert-flp30-c"
+
 #include <cmath>
-#include <QList>
+#include <QtCore/QList>
+#include <QtCore/QDebug>
 
 class Tasks final { public:
+
+    [[deprecated("test only")]] Tasks() {
+        qInfo() << Tasks::t19(5);
+        qInfo() << Tasks::t22(10);
+        qInfo() << Tasks::t25(1.0f);
+        qInfo() << Tasks::t28();
+        qInfo() << Tasks::t3({1.5f, -2.1f, 3.0f, -4.0f, 5.0f});
+    }
+
     static float t19(int n) {
         if (n < 0) return NAN;
-        for (float i = 0, j = 0; i <= n; j = sinf(tanf(i / 2.0f)), i += 1.0f)
+        for (float i = 0, j = 0; i <= (float) n; j = sinf(tanf(i / 2.0f)), i += 1.0f)
             if (j < 0) return j;
         return NAN;
     }
 
     static float t22(float n) {
         if (n < 0) return NAN;
-        int k = 0;
-        for (int i = 0, j; (j = powf(3, i)) < n; k = j, i++);
+        float k = 0;
+        for (float i = 0, j; (j = powf(3, i)) < n; k = j, i += 1.0f);
         return k;
     }
 
@@ -26,6 +38,15 @@ class Tasks final { public:
     }
 
     static QList<float> t28() {
-        // TODO
+        QList<float> list;
+        for (float i = 5; i >= -12; list.push_back(sinf(i) - 5.0f * cosf(i - 2.0f)), i -= 1.2f);
+        return list;
+    }
+
+    static QList<float> t3(const QList<float>& input) {
+        QList<float> output;
+        for (float i : input)
+            output.push_back(fabsf(i));
+        return output;
     }
 };
--- p3/main.cpp
+++ p3/main.cpp
@@ -1,16 +1,9 @@
 
 #include "Server.hpp"
 #include <QtCore/QCoreApplication>
-#include "Tasks.hpp"
 
 int main(int argc, char** argv) {
-    qInfo() << Tasks::t19(5);
-    qInfo() << Tasks::t22(10);
-    qInfo() << Tasks::t25(1.0f);
-
-
-//    QCoreApplication app(argc, argv);
-//    Server server;
-//    return QCoreApplication::exec();
-    return 0;
+    QCoreApplication app(argc, argv);
+    Server server;
+    return QCoreApplication::exec();
 }
